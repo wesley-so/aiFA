c0626b56e7ee7e6741039331bdd9673327da2938
Wesley So <wesleyso0614@gmail.com>
2023-01-17 18:46:57 +0800

    Configure Login and Register APIs
    Ref #7

--- server/aifa/routers/user.py
+++ server/aifa/routers/user.py
@@ -8,32 +8,33 @@ import os
 router = APIRouter(prefix="/user", tags=["user"])
 MONGO_HOST = os.getenv("MONGO_HOST")
 MONGO_PORT = os.getenv("MONGO_PORT")
-conn = MongoClient(f"mongodb://{MONGO_HOST}:{MONGO_PORT}")
-db = conn["aifa"]
-userCol = db["user"]
+conn = MongoClient(f"mongodb://{MONGO_HOST}:{MONGO_PORT}/")
+db = conn.aifa
+userCol = db.user
+session = db.session
 
 
 class LoginModel(BaseModel):
     username: str = Field(title="Username", max_length=50)
-    password: str = Field(title="User password", max_length=20)
+    password: bytes = Field(title="User password", max_length=20)
 
 
 class RegisterModel(BaseModel):
     username: str = Field(title="Username", max_length=50)
     email: str = Field(title="User email", max_length=254)
-    password: str = Field(title="User password", max_length=20)
-    passwordConfirm: str = Field(title="Password confirmation", max_length=20)
+    password: bytes = Field(title="User password", max_length=20)
+    passwordConfirm: bytes = Field(title="Password confirmation", max_length=20)
 
 
 class UserModel(BaseModel):
     username: str = Field(title="Username", max_length=50)
     email: str | None = Field(title="User email", max_length=254)
-    password: str = Field(title="User password", max_length=20)
+    password: bytes = Field(title="User password", max_length=20)
 
 
 # Hash password function
-def hash_password(password_bytes, salt):
-    return bcrypt.hashpw(password=password_bytes, salt=salt)
+def hash_password(password, salt):
+    return bcrypt.hashpw(password=password, salt=salt)
 
 
 @router.post("/login", response_class=JSONResponse)
@@ -47,16 +48,18 @@ async def login(login: LoginModel):
     password = login.password
 
     query = {"username": username}
-    userQuery = userCol.find(query)
-
+    projection = {"_id": 0, "username": 1, "password": 1}
+    userQuery = userCol.find(query, projection)
+    for i in userQuery:
+        user_username = i["username"]
+        user_password = i["password"]
     # Check if the username is empty
-    if userQuery.count() == 0:
+    if len(user_username) == 0:
         return JSONResponse(
             status_code=400, content="Username or password incorrect! Please try again."
         )
-
     # Check password match or not
-    if bcrypt.checkpw(password, userQuery.password):
+    if bcrypt.checkpw(password, user_password):
         return JSONResponse(status_code=200, content="Login successfully!")
     else:
         return JSONResponse(
@@ -88,18 +91,19 @@ async def register(register: RegisterModel):
     # Check password and passwordConfirm
     if password != passwordConfirm:
         return JSONResponse(status_code=400, content="Password do not match!")
-
-    # Check if the collection have this username
-    for i in userCol.find():
-        if username == i.username:
+    if userCol.count_documents({}) != 0:
+        # if userCol.find({"username": {"$eq": {username}}}) > 0:
+        if userCol.count_documents({"username": {username}}) > 0:
             return JSONResponse(
                 status_code=400,
                 content="Username repeated! Please create another username.",
             )
-        if email == i.email:
+        # if userCol.find({"email": {"$eq": {email}}}).count() > 0:
+        if userCol.count_documents({"email": {email}}) > 0:
             return JSONResponse(
                 status_code=400, content="Email repeated! Please type another email."
             )
+
     salt = bcrypt.gensalt(16)
     hashed = hash_password(password, salt)
     newUser = {"username": username, "email": email, "password": hashed, "salt": salt}
@@ -111,44 +115,66 @@ async def register(register: RegisterModel):
 
 
 @router.post("/{userId}", response_class=JSONResponse)
-async def edit(edit: UserModel, userId: int = Path(title="User ID")):
+async def edit(edit: UserModel, userId: str = Path(title="User ID")):
     query = {"_id": userId}
-    userInfo = userCol.find(query)
+    projection = {"_id": 0, "username": 1, "password": 1}
+    userInfo = userCol.find(query, projection)
+    for i in userInfo:
+        username = i["username"]
+        password = i["password"]
+
     new_username = edit.username
     new_password = edit.password
+    new_salt = bcrypt.gensalt(16)
+    new_hashed_password = hash_password(new_password, new_salt)
 
+    # Check if username and password is empty
     if new_username.__len__ == 0 and new_password.__len__ == 0:
         return JSONResponse(
             status_code=400, content="Some fields are missing or invalid!"
         )
+    # Check if user is logged in
+    # if userInfo.count() == 0:
+    #     return JSONResponse(
+    #         status_code=401, content="User is unauthorized to update account."
+    #     )
 
-    if userInfo.count() == 0:
+    # Check if username is repeated with the original one
+    if username == new_username:
         return JSONResponse(
-            status_code=401, content="User is unauthorized to update account."
+            status_code=400,
+            content="Username repeated! Please change another username.",
         )
-    else:
-        if userInfo.username == new_username:
-            return JSONResponse(
-                status_code=400,
-                content="Username repeated! Please change another username.",
-            )
-        if bcrypt.checkpw(new_password, userInfo.password):
-            return JSONResponse(
-                status_code=400,
-                content="Password repeated! Please change another password.",
-            )
-        newValues = {"$set": {"username": new_username, "password": new_password}}
-        userCol.update_one(query, newValues)
+    # Check if the password is repeated with the original one
+    if bcrypt.checkpw(new_password, password):
+        return JSONResponse(
+            status_code=400,
+            content="Password repeated! Please change another password.",
+        )
+
+    # Update username and hashed password in
+    newValues = {
+        "$set": {
+            "username": new_username,
+            "password": new_hashed_password,
+            "salt": new_salt,
+        }
+    }
+    userCol.update_one(query, newValues)
+    return JSONResponse(status_code=200, content="User info updated successfully!")
 
 
 @router.get("/{userId}", response_class=JSONResponse)
-async def info(userId: int = Path(title="User ID")):
-    query = {"_id": userId}
-    userInfo = userCol.find(query)
-    username = userInfo.username
-    email = userInfo.email
+async def info(userId: str = Path(title="User ID")):
+    userInfo = userCol.find(
+        {"_id": f'ObjectId("{userId}")'}, {"_id": 0, "username": 1, "email": 1}
+    )
+    result = list(userInfo)
+    for i in userInfo:
+        username = i["username"]
+        email = i["email"]
 
-    if userInfo.count() == 0:
+    if len(result) == 0:
         return JSONResponse(
             status_code=401, content="User is unauthorized to get account information."
         )
