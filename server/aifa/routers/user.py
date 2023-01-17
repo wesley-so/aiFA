d5e84a6c06b6fd14f23fe8db1cb476d6761cac1e
Wesley So <wesleyso0614@gmail.com>
2023-01-17 19:52:40 +0800

    Configure login register and get info APIs
    Ref #7

--- server/aifa/routers/user.py
+++ server/aifa/routers/user.py
@@ -1,4 +1,5 @@
 import bcrypt
+from bson.objectid import ObjectId
 from fastapi import APIRouter, Path
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
@@ -39,11 +40,6 @@ def hash_password(password, salt):
 
 @router.post("/login", response_class=JSONResponse)
 async def login(login: LoginModel):
-    if login.username.__len__ == 0 or login.password.__len__ == 0:
-        return JSONResponse(
-            status_code=400, content="Some fields are missing or invalid!"
-        )
-
     username = login.username
     password = login.password
 
@@ -51,19 +47,27 @@ async def login(login: LoginModel):
     projection = {"_id": 0, "username": 1, "password": 1}
     userQuery = userCol.find(query, projection)
     for i in userQuery:
-        user_username = i["username"]
         user_password = i["password"]
-    # Check if the username is empty
-    if len(user_username) == 0:
+
+    # Check if the username matches the database data
+    if userCol.count_documents(query) == 0:
         return JSONResponse(
-            status_code=400, content="Username or password incorrect! Please try again."
+            status_code=400,
+            content={
+                "success": False,
+                "message": "Username or password incorrect! Please try again.",
+            },
         )
     # Check password match or not
     if bcrypt.checkpw(password, user_password):
         return JSONResponse(status_code=200, content="Login successfully!")
     else:
         return JSONResponse(
-            status_code=400, content="Username or password incorrect! Please try again."
+            status_code=400,
+            content={
+                "success": False,
+                "message": "Username or password incorrect! Please try again.",
+            },
         )
 
 
@@ -75,13 +79,17 @@ async def logout():
 @router.post("/register", response_class=JSONResponse)
 async def register(register: RegisterModel):
     if (
-        register.username.__len__ == 0
-        or register.email.__len__ == 0
-        or register.password.__len__ == 0
-        or register.passwordConfirm.__len__ == 0
+        register.username.count() == 0
+        or register.email.count() == 0
+        or register.password.count() == 0
+        or register.passwordConfirm.count() == 0
     ):
         return JSONResponse(
-            status_code=400, content="Some fields are missing or invalid!"
+            status_code=400,
+            content={
+                "success": False,
+                "message": "Some fields are missing or invalid!",
+            },
         )
     username = register.username
     email = register.email
@@ -90,18 +98,29 @@ async def register(register: RegisterModel):
 
     # Check password and passwordConfirm
     if password != passwordConfirm:
-        return JSONResponse(status_code=400, content="Password do not match!")
+        return JSONResponse(
+            status_code=400,
+            content={"success": False, "message": "Password do not match!"},
+        )
+
     if userCol.count_documents({}) != 0:
         # if userCol.find({"username": {"$eq": {username}}}) > 0:
         if userCol.count_documents({"username": {username}}) > 0:
             return JSONResponse(
                 status_code=400,
-                content="Username repeated! Please create another username.",
+                content={
+                    "success": False,
+                    "message": "Username repeated! Please create another username.",
+                },
             )
         # if userCol.find({"email": {"$eq": {email}}}).count() > 0:
         if userCol.count_documents({"email": {email}}) > 0:
             return JSONResponse(
-                status_code=400, content="Email repeated! Please type another email."
+                status_code=400,
+                content={
+                    "success": False,
+                    "message": "Email repeated! Please type another email.",
+                },
             )
 
     salt = bcrypt.gensalt(16)
@@ -110,7 +129,10 @@ async def register(register: RegisterModel):
     insertUser = userCol.insert_one(newUser)
     return JSONResponse(
         status_code=201,
-        content=f"Account created successfully! ID: {insertUser.inserted_id}",
+        content={
+            "success": True,
+            "message": f"Account created successfully! ID: {insertUser.inserted_id}",
+        },
     )
 
 
@@ -118,20 +140,23 @@ async def register(register: RegisterModel):
 async def edit(edit: UserModel, userId: str = Path(title="User ID")):
     query = {"_id": userId}
     projection = {"_id": 0, "username": 1, "password": 1}
-    userInfo = userCol.find(query, projection)
+    userInfo = userCol.find_one(query, projection)
     for i in userInfo:
         username = i["username"]
         password = i["password"]
-
     new_username = edit.username
     new_password = edit.password
     new_salt = bcrypt.gensalt(16)
     new_hashed_password = hash_password(new_password, new_salt)
 
     # Check if username and password is empty
-    if new_username.__len__ == 0 and new_password.__len__ == 0:
+    if new_username.count() == 0 and new_password.count() == 0:
         return JSONResponse(
-            status_code=400, content="Some fields are missing or invalid!"
+            status_code=400,
+            content={
+                "success": False,
+                "message": "Some fields are missing or invalid!",
+            },
         )
     # Check if user is logged in
     # if userInfo.count() == 0:
@@ -143,13 +168,19 @@ async def edit(edit: UserModel, userId: str = Path(title="User ID")):
     if username == new_username:
         return JSONResponse(
             status_code=400,
-            content="Username repeated! Please change another username.",
+            content={
+                "success": False,
+                "message": "Username repeated! Please change another username.",
+            },
         )
     # Check if the password is repeated with the original one
     if bcrypt.checkpw(new_password, password):
         return JSONResponse(
             status_code=400,
-            content="Password repeated! Please change another password.",
+            content={
+                "success": False,
+                "message": "Password repeated! Please change another password.",
+            },
         )
 
     # Update username and hashed password in
@@ -161,24 +192,24 @@ async def edit(edit: UserModel, userId: str = Path(title="User ID")):
         }
     }
     userCol.update_one(query, newValues)
-    return JSONResponse(status_code=200, content="User info updated successfully!")
+    return JSONResponse(
+        status_code=200,
+        content={"success": True, "message": "User info updated successfully!"},
+    )
 
 
 @router.get("/{userId}", response_class=JSONResponse)
 async def info(userId: str = Path(title="User ID")):
-    userInfo = userCol.find(
-        {"_id": f'ObjectId("{userId}")'}, {"_id": 0, "username": 1, "email": 1}
-    )
-    result = list(userInfo)
+    userId = ObjectId(userId)
+    query = {"_id": userId}
+    projection = {"_id": 0, "username": 1, "email": 1}
+    userInfo = userCol.find(query, projection)
     for i in userInfo:
         username = i["username"]
         email = i["email"]
 
-    if len(result) == 0:
-        return JSONResponse(
-            status_code=401, content="User is unauthorized to get account information."
-        )
-    else:
-        return JSONResponse(
-            status_code=200, content=f"Username: {username} \n Email: {email}"
-        )
+    return JSONResponse(
+        status_code=200,
+        content={"success": True, "message": f"Username: {username}, Email: {email}"},
+        # headers={"username": username, "email": email}
+    )
