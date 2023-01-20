63b5ee3c8c6479d275ed0864b1d7b64a0e8fb3b9
Sikyin Tang <thomas2d19@gmail.com>
2023-01-20 10:52:07 +0800

    Add UserContextProvider and useUser hooks for future use

---
+++ web/src/hooks/useUser.ts
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+const useUser = () => {
+  const props = useContext(UserContext);
+
+  if (!props) {
+    throw new Error("`useUser` hooks cannot use outside `UserContext`.");
+  }
+
+  return [props.user, props.loginStatus, props.fetchLogin, props.fetchLogout];
+};
+
+export default useUser;
