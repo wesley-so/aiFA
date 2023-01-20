e822f98cac1c2b2925549f133f7da61edef3e64c
Sikyin Tang <thomas2d19@gmail.com>
2023-01-20 10:51:45 +0800

    Create user context

---
+++ web/src/context/UserContext.ts
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+import LoginStatus from "../models/LoginStatus";
+import User from "../models/User";
+
+interface UserContextProps {
+  user?: User;
+  loginStatus: LoginStatus;
+  fetchLogin: (username: string, password: string) => void;
+  fetchLogout: () => void;
+}
+
+const UserContext = createContext<UserContextProps | undefined>(undefined);
+
+export default UserContext;
