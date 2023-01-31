ca6e7f200950fd82f4990a90a08c2c128821fcdf
Wesley So <wesleyso0614@gmail.com>
2023-02-01 00:53:21 +0800

    Modify login and register API
    Ref #7

--- server/aifa/routers/user.py
+++ server/aifa/routers/user.py
@@ -1,11 +1,12 @@
 import bcrypt
 from bson.objectid import ObjectId
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Depends, Path
 from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel, Field
 from pymongo import MongoClient
 import os
-from ..service.jwt import signJWT, expireJWT
+from ..service.jwt import decodeJWT, expireJWT, signJWT
 
 router = APIRouter(prefix="/user", tags=["user"])
 MONGO_HOST = os.getenv("MONGO_HOST")
@@ -14,6 +15,14 @@ conn = MongoClient(f"mongodb://{MONGO_HOST}:{MONGO_PORT}/")
 db = conn.aifa
 userCol = db.user
 session = db.session
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+class UserModel(BaseModel):
+    username: str
+    userId: str
+    email: str
 
 
 class LoginModel(BaseModel):
@@ -25,18 +34,40 @@ class RegisterModel(BaseModel):
     username: str = Field(title="Username", max_length=50)
     email: str = Field(title="User email", max_length=254)
     password: bytes = Field(title="User password", max_length=20)
-    passwordConfirm: bytes = Field(title="Password confirmation", max_length=20)
+    password_confirm: bytes = Field(title="Password confirmation", max_length=20)
 
 
-class UserModel(BaseModel):
+class UserEditModel(BaseModel):
     username: str = Field(title="Username", max_length=50)
     email: str | None = Field(title="User email", max_length=254)
     password: bytes = Field(title="User password", max_length=20)
 
 
 # Hash password function
-def hash_password(password, salt):
+async def hash_password(password, salt):
     return bcrypt.hashpw(password=password, salt=salt)
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = JSONResponse(
+        status_code=401,
+        content="Could not validate credential",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = decodeJWT(token)
+        userId = payload["userId"]
+        if userId is None:
+            raise credentials_exception
+    except Exception as error:
+        return error
+    user_data = userCol.find_one({"_id": ObjectId(userId)})
+    if user_data is None:
+        raise credentials_exception
+    user = UserModel(
+        username=user_data["username"], userId=userId, email=user_data["email"]
+    )
+    return user
 
 
 @router.post("/login", response_class=JSONResponse)
@@ -48,8 +79,7 @@ async def login(login: LoginModel):
         return JSONResponse(
             status_code=400,
             content={
-                "success": False,
-                "message": "Some fields are missing or invalid!",
+                "error": "Some fields are missing or invalid!",
             },
         )
 
@@ -62,38 +92,31 @@ async def login(login: LoginModel):
         return JSONResponse(
             status_code=400,
             content={
-                "success": False,
-                "message": "Username or password incorrect! Please try again.",
+                "error": "Username or password incorrect! Please try again.",
             },
         )
     else:
-        userPassword = userQuery["password"]
+        user_password = userQuery["password"]
         userId = str(userQuery["_id"])
 
     # Check password match or not
-    if bcrypt.checkpw(password, userPassword):
+    if bcrypt.checkpw(password, user_password):
         token = signJWT(userId)
         session.insert_one({"token": token}, True)
-        return JSONResponse(status_code=200, content="Login successfully!")
+        return JSONResponse(status_code=200, content={"token": token})
     else:
         # Password incorrect
         return JSONResponse(
             status_code=400,
             content={
-                "success": False,
-                "message": "Username or password incorrect! Please try again.",
+                "error": "Username or password incorrect! Please try again.",
             },
         )
 
 
 @router.post("/logout", response_class=JSONResponse)
 async def logout(token):
-    print(token)
     expireJWT(token)
-    return JSONResponse(
-        status_code=200,
-        content={"success": True, "message": "Logout successfully!"},
-    )
 
 
 @router.post("/register", response_class=JSONResponse)
@@ -101,71 +124,64 @@ async def register(register: RegisterModel):
     username = register.username
     email = register.email
     password = register.password
-    passwordConfirm = register.passwordConfirm
+    password_confirm = register.password_confirm
     if (
         len(username) == 0
         or len(email) == 0
         or len(password) == 0
-        or len(passwordConfirm) == 0
+        or len(password_confirm) == 0
     ):
         return JSONResponse(
             status_code=400,
             content={
-                "success": False,
-                "message": "Some fields are missing or invalid!",
+                "error": "Some fields are missing or invalid!",
             },
         )
 
     # Check password and passwordConfirm
-    if password != passwordConfirm:
+    if password != password_confirm:
         return JSONResponse(
             status_code=400,
-            content={"success": False, "message": "Password do not match!"},
+            content={"error": "Password do not match!"},
         )
 
-    if userCol.count_documents({}) != 0:
-        # if userCol.find({"username": {"$eq": {username}}}) > 0:
-        if userCol.count_documents({"username": {username}}) > 0:
-            return JSONResponse(
-                status_code=400,
-                content={
-                    "success": False,
-                    "message": "Username repeated! Please create another username.",
-                },
-            )
-        # if userCol.find({"email": {"$eq": {email}}}).count() > 0:
-        if userCol.count_documents({"email": {email}}) > 0:
-            return JSONResponse(
-                status_code=400,
-                content={
-                    "success": False,
-                    "message": "Email repeated! Please type another email.",
-                },
-            )
+    if userCol.find_one({"username": username}) is not None:
+        return JSONResponse(
+            status_code=400,
+            content={
+                "error": "Username repeated! Please create another username.",
+            },
+        )
+    if userCol.find_one({"email": email}) is not None:
+        return JSONResponse(
+            status_code=400,
+            content={
+                "error": "Email repeated! Please type another email.",
+            },
+        )
 
     salt = bcrypt.gensalt(16)
     hashed = hash_password(password, salt)
-    newUser = {"username": username, "email": email, "password": hashed, "salt": salt}
-    insertUser = userCol.insert_one(newUser)
-    userId = insertUser.inserted_id
+    new_user = {"username": username, "email": email, "password": hashed, "salt": salt}
+    insert_user = userCol.insert_one(new_user)
+    userId = insert_user.inserted_id
 
     return JSONResponse(
         status_code=201,
         content={
-            "success": True,
             "message": f"Account created successfully! ID: {userId}",
         },
     )
 
 
 @router.post("/{userId}", response_class=JSONResponse)
-async def edit(edit: UserModel, userId: str = Path(title="User ID")):
+async def edit(edit: UserEditModel, userId: str = Path(title="User ID")):
     userId = ObjectId(userId)
     query = {"_id": userId}
     projection = {"_id": 0, "username": 1, "password": 1}
-    userInfo = userCol.find_one(query, projection)
-    username = userInfo["username"]
-    password = userInfo["password"]
+    user_info = userCol.find_one(query, projection)
+    username = user_info["username"]
+    password = user_info["password"]
 
     new_username = edit.username
     new_password = edit.password
@@ -173,12 +189,11 @@ async def edit(edit: UserModel, userId: str = Path(title="User ID")):
     new_hashed_password = hash_password(new_password, new_salt)
 
     # Check if username and password is empty
-    if new_username.count() == 0 and new_password.count() == 0:
+    if len(new_username) == 0 and len(new_password) == 0:
         return JSONResponse(
             status_code=400,
             content={
-                "success": False,
-                "message": "Some fields are missing or invalid!",
+                "error": "Some fields are missing or invalid!",
             },
         )
     # Check if user is logged in
@@ -192,8 +207,7 @@ async def edit(edit: UserModel, userId: str = Path(title="User ID")):
         return JSONResponse(
             status_code=400,
             content={
-                "success": False,
-                "message": "Username repeated! Please change another username.",
+                "error": "Username repeated! Please change another username.",
             },
         )
     # Check if the password is repeated with the original one
@@ -201,24 +215,20 @@ async def edit(edit: UserModel, userId: str = Path(title="User ID")):
         return JSONResponse(
             status_code=400,
             content={
-                "success": False,
-                "message": "Password repeated! Please change another password.",
+                "error": "Password repeated! Please change another password.",
             },
         )
 
     # Update username and hashed password in
-    newValues = {
+    new_values = {
         "$set": {
             "username": new_username,
             "password": new_hashed_password,
             "salt": new_salt,
         }
     }
-    userCol.update_one(query, newValues)
-    return JSONResponse(
-        status_code=200,
-        content={"success": True, "message": "User info updated successfully!"},
-    )
+    userCol.update_one(query, new_values)
+    return JSONResponse(status_code=200)
 
 
 @router.get("/{userId}", response_class=JSONResponse)
@@ -226,11 +236,11 @@ async def info(userId: str = Path(title="User ID")):
     userId = ObjectId(userId)
     query = {"_id": userId}
     projection = {"_id": 0, "username": 1, "email": 1}
-    userInfo = userCol.find_one(query, projection)
-    username = userInfo["username"]
-    email = userInfo["email"]
+    user_info = userCol.find_one(query, projection)
+    username = user_info["username"]
+    email = user_info["email"]
 
     return JSONResponse(
         status_code=200,
-        content={"success": True, "message": f"Username: {username}, Email: {email}"},
+        content={"username": username, "email": email},
     )
--- server/aifa/service/jwt.py
+++ server/aifa/service/jwt.py
@@ -1,9 +1,5 @@
-from bson import json_util, ObjectId
-from datetime import datetime, timedelta
-from fastapi.responses import JSONResponse
+from datetime import datetime
 import jwt
-from jwt.exceptions import ExpiredSignatureError
-import json
 from pymongo import MongoClient
 import secrets
 import os
@@ -18,45 +14,20 @@ session = db.session
 jwt_secret = os.getenv("JWT_SECRET")
 
 
-def signJWT(userId):
+def signJWT(userId, expires_delta: int | None = None):
     nbf = int(datetime.now().strftime("%Y%m%d%H%M%S"))
-    exp = nbf + 259200  # 3 days to expire
+    if expires_delta:
+        exp = nbf + expires_delta
+    else:
+        exp = nbf + 259200  # 3 days to expire
     sessionId = secrets.token_urlsafe(16)
     payload = {"userId": userId, "nbf": nbf, "exp": exp, "sessionId": sessionId}
-    print(payload)
     return jwt.encode(payload, jwt_secret, algorithm="HS256")
 
 
-def checkJWT(token):
-    try:
-        payload = jwt.decode(token, jwt_secret, algorithms="HS256")
-        return payload
-
-    except ExpiredSignatureError as error:
-        return JSONResponse(
-            status_code=400,
-            content={
-                "success": False,
-                "message": f"Unable to decode your token, error:{error}",
-            },
-        )
+def decodeJWT(token):
+    return jwt.decode(token, jwt_secret, algorithms="HS256")
 
 
 def expireJWT(token):
-    try:
-        jwt.decode(token, jwt_secret, algorithms="HS256")
-        session.remove({"token": token}, {"justOne": True})
-    except ExpiredSignatureError as error:
-        return JSONResponse(
-            status_code=400,
-            content={
-                "success": False,
-                "message": f"Unable to decode your token, error:{error}",
-            },
-        )
-
-
-if __name__ == "__main__":
-    signJWT()
-    checkJWT()
-    expireJWT()
+    session.remove({"token": token}, {"justOne": True})
