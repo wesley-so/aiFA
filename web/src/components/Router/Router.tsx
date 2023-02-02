a49f89e2d3c19151f7127d650931391885e86c77
Wesley So <wesleyso0614@gmail.com>
2023-02-03 01:53:39 +0800

    Add Router path
    - HomePage
    - LoginPage
    
    Ref #8

--- web/src/components/Router/Router.tsx
+++ web/src/components/Router/Router.tsx
@@ -6,13 +6,21 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import App from "../App/App";
+import HomePage from "../HomePage/HomePage";
+import LoginPage from "../LoginPage/LoginPage";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="app" element={<App />}></Route>
+      <Route path="login" element={<LoginPage />}></Route>
+    </>
+  )
 );
 
 const Router: FC<{}> = () => {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default Router;
