5c3ceafd23bad9c21a14a8c7fb4827ccf48efcec
Wesley So <wesleyso0614@gmail.com>
2023-02-03 01:54:27 +0800

    Add HomePage basic UI
    Ref #8

---
+++ web/src/components/HomePage/HomePage.tsx
@@ -0,0 +1,24 @@
+import { Grid, Typography } from "@mui/material";
+import { FC } from "react";
+import NavigationBar from "../NavigationBar/NavigationBar";
+
+const HomePage: FC<{}> = () => {
+  return (
+    <>
+      <NavigationBar></NavigationBar>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        minHeight="100vh"
+      >
+        <Typography variant="h1">aiFA</Typography>
+        <Typography variant="body1">This is the description</Typography>
+      </Grid>
+    </>
+  );
+};
+
+export default HomePage;
