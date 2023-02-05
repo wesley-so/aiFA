47f8e0e0c10cab3626fbe22222ccca9daa18902f
Wesley So <wesleyso0614@gmail.com>
2023-02-06 02:28:20 +0800

    Refactor router
    Ref #8

--- web/src/components/Router/Router.tsx
+++ web/src/components/Router/Router.tsx
@@ -1,10 +1,6 @@
-import { FC } from "react";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { FC, useCallback } from "react";
+import { BrowserRouter, redirect, Route, Routes } from "react-router-dom";
+import useUser from "../../hooks/useUser";
 import App from "../App/App";
 import Dashboard from "../Dashboard/Dashboard";
 import HomePage from "../HomePage/HomePage";
@@ -12,21 +8,47 @@ import LoginPage from "../LoginPage/LoginPage";
 import RegisterPage from "../RegisterPage/RegisterPage";
 import UserProfilePage from "../UserProfilePage/UserProfilePage";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="app" element={<App />}></Route>
-      <Route path="dashboard" element={<Dashboard />}></Route>
-      <Route path="login" element={<LoginPage />}></Route>
-      <Route path="register" element={<RegisterPage />}></Route>
-      <Route path="profile" element={<UserProfilePage />}></Route>
-    </>
-  )
-);
-
 const Router: FC = () => {
-  return <RouterProvider router={router} />;
+  const {
+    loginStatus: { isLoggedIn },
+  } = useUser();
+
+  const redirectLoggeedInUser = useCallback(() => {
+    if (isLoggedIn) {
+      redirect("/dashboard");
+    }
+  }, [isLoggedIn]);
+
+  const requireLogin = useCallback(() => {
+    if (!isLoggedIn) {
+      redirect("/login");
+    }
+  }, [isLoggedIn]);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="app" element={<App />}></Route>
+        <Route
+          path="dashboard"
+          loader={requireLogin}
+          element={<Dashboard />}
+        ></Route>
+        <Route
+          path="login"
+          loader={redirectLoggeedInUser}
+          element={<LoginPage />}
+        ></Route>
+        <Route path="register" element={<RegisterPage />}></Route>
+        <Route
+          path="profile"
+          loader={requireLogin}
+          element={<UserProfilePage />}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
