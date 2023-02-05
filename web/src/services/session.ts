3e949ceea29af57a49ee81c2512fa4400b212b80
Wesley So <wesleyso0614@gmail.com>
2023-02-06 02:29:04 +0800

    Create session related codes
    Ref #8

---
+++ web/src/services/session.ts
@@ -0,0 +1,13 @@
+import Cookies from "js-cookie";
+
+export const getSessionToken = () => {
+  return Cookies.get("session");
+};
+
+export const setSessionToken = (token: string) => {
+  return Cookies.set("session", token, { path: "/" });
+};
+
+export const removeSessionToken = () => {
+  return Cookies.remove("session", { path: "/" });
+};
