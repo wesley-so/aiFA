70e0e6472eeadcce5733f2b04301187a95842584
Wesley So <wesleyso0614@gmail.com>
2023-03-03 15:59:16 +0800

    Refine grab_daily_stock, decision_forest and markdown files
    - install new dependencies for AI models
    
    Ref #16

--- cron/aifa_cron/jobs/grab_daily_stock.py
+++ cron/aifa_cron/jobs/grab_daily_stock.py
@@ -7,41 +7,36 @@ from ..database import init_database, insert_stock
 init_database()
 
 
-def grab_daily_stock():
-    grabList = [
-        # "AAPL",
-        "MSFT",
-        # "GOOG",
-        # "AMZN",
-        # "TSLA",
-        # "META",
-        # "BABA",
-        # "ORCL",
-        # "CSCO",
-        # "NVDA",
-        # "JNJ",
-        # "TSM",
-        # "WMT",
-        # "PFE",
-        # "COST",
-        # "KO",
-        # "UNH",
-        # "HSBC",
-        # "QCOM",
-        # "AMD",
-    ]
+def grab_daily_stock(symbol: str):
 
     api_key = getenv("FMP_API_KEY")
-    for k in grabList:
-        r = requests.get(
-            f"https://financialmodelingprep.com/api/v3/historical-chart/1min/{k}?apikey={api_key}"  # noqa: E501
-        )
-        price_list = r.json()
-        for i in price_list:
-            i["symbol"] = k
-        # print(price_list)
-        insert_stock(price_list)
+    r = requests.get(
+        f"https://financialmodelingprep.com/api/v3/historical-chart/1min/{symbol}?apikey={api_key}"
+    )
+    price_list = r.json()
+    for i in price_list:
+        i["symbol"] = symbol
+    insert_stock(price_list)
 
 
 if __name__ == "__main__":
-    grab_daily_stock()
+    # grab_daily_stock("AAPL")
+    grab_daily_stock("MSFT")
+    # grab_daily_stock("GOOG")
+    # grab_daily_stock("AMZN")
+    # grab_daily_stock("TSLA")
+    # grab_daily_stock("META")
+    # grab_daily_stock("BABA")
+    # grab_daily_stock("ORCL")
+    # grab_daily_stock("CSCO")
+    # grab_daily_stock("NVDA")
+    # grab_daily_stock("JNJ")
+    # grab_daily_stock("TSM")
+    # grab_daily_stock("WMT")
+    # grab_daily_stock("PFE")
+    # grab_daily_stock("COST")
+    # grab_daily_stock("KO")
+    # grab_daily_stock("UNH")
+    # grab_daily_stock("HSBC")
+    # grab_daily_stock("QCOM")
+    # grab_daily_stock("AMD")
