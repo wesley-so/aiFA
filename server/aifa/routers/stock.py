462cd93569491f1cbf76a61feaa939e34719122f
Sikyin Tang <thomas2d19@gmail.com>
2023-04-11 00:38:21 +0800

    Add API authentication

--- server/aifa/routers/stock.py
+++ server/aifa/routers/stock.py
@@ -2,10 +2,10 @@ import base64
 from os import getenv
 
 import boto3
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import JSONResponse
 
-from aifa.dependencies.session import get_session_token
+from aifa.dependencies.session import get_session_user
 from aifa.models.stock import StockModel
 from aifa.services.stock import grab_daily_ohlcv
 
@@ -13,7 +13,7 @@ router = APIRouter(prefix="/stock", tags=["stock"])
 
 
 @router.post("/daily", response_class=JSONResponse)
-async def daily(stock: StockModel):
+async def daily(stock: StockModel, user=Depends(get_session_user)):
     stock_data = await grab_daily_ohlcv(stock.symbol)
     if not stock_data:
         raise HTTPException(status_code=401, detail="User unauthorized.")
@@ -21,29 +21,25 @@ async def daily(stock: StockModel):
 
 
 @router.post("/graph")
-async def grab_graph(stock: StockModel):
-    token = get_session_token()
-    if token:
-        s3_client = boto3.client(
-            "s3",
-            endpoint_url=getenv("S3_ENDPOINT"),
-            aws_access_key_id=getenv("S3_ACCESS_KEY"),
-            aws_secret_access_key=getenv("S3_SECRET_KEY"),
-        )
-        bucket_name = "stockgraph"
-        try:
-            file_byte = s3_client.get_object(
-                Bucket=bucket_name, Key=f"{stock.symbol}.png"
-            )["Body"].read()
-            encoded_string = base64.b64encode(file_byte).decode("utf-8")
-            return JSONResponse(status_code=200, content=encoded_string)
-        except Exception as error:
-            if hasattr(error, "message"):
-                raise HTTPException(
-                    status_code=error.message["response"]["Error"]["Code"],
-                    detail=error.message["response"]["Error"]["Message"],
-                )
-            else:
-                raise HTTPException(status_code=500, detail=str(error))
-    else:
-        raise HTTPException(status_code=401, detail="User unauthorized.")
+async def grab_graph(stock: StockModel, user=Depends(get_session_user)):
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url=getenv("S3_ENDPOINT"),
+        aws_access_key_id=getenv("S3_ACCESS_KEY"),
+        aws_secret_access_key=getenv("S3_SECRET_KEY"),
+    )
+    bucket_name = "stockgraph"
+    try:
+        file_byte = s3_client.get_object(Bucket=bucket_name, Key=f"{stock.symbol}.png")[
+            "Body"
+        ].read()
+        encoded_string = base64.b64encode(file_byte).decode("utf-8")
+        return JSONResponse(status_code=200, content=encoded_string)
+    except Exception as error:
+        if hasattr(error, "message"):
+            raise HTTPException(
+                status_code=error.message["response"]["Error"]["Code"],
+                detail=error.message["response"]["Error"]["Message"],
+            )
+        else:
+            raise HTTPException(status_code=500, detail=str(error))
